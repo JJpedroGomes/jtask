3509dd30c765acacce1c1a84156e3f630ba2d1e3
joao.gomes <joao.gomes2@tjce.jus.br>
2024-04-16 22:26:42 -0300

    [#44] Refactor logic for update events on frontend

--- src/main/webapp/js/todo.js
+++ src/main/webapp/js/todo.js
@@ -1,45 +1,33 @@
-const form = document.getElementById("modal_form");
+let form = document.getElementById("modal_form");
 const taskName = document.getElementById("task_title");
 const taskDescription = document.getElementById("task_description");
 const taskDueDate = document.getElementById("task_due_date");
 const lane = document.getElementById("todo_lane");
 const btnSubmit = document.getElementById("modal_submit_btn");
+const modalBackground = document.querySelector(".modal_background");
 
-// Open modal
-document.getElementById("modal_button").addEventListener("click", () => {
-    document.querySelector(".modal_background").style.display = "flex";
-    form.removeEventListener("submit", addUpdateTaskEventListener);
-    form.addEventListener("submit", addCreateTaskEventListener);
-});
 
-//Close modal
-document.querySelector(".close-btn").addEventListener("click", () => {
+// Event Listeners
+document.getElementById("modal_button").addEventListener("click", openModalForCreate);
+document.querySelector(".close-btn").addEventListener("click", closeModal);
+
+// Function to open modal for creating a task
+function openModalForCreate() {
+    modalBackground.style.display = "flex";
+    form.removeEventListener("submit", handleUpdateTask);
+    form.addEventListener("submit", handleCreateTask);
     resetFormInputs();
-    document.querySelector(".modal_background").style.display = "none";
-});
+}
 
-function addTaskToLane(task) {
-    const newTask = document.createElement("p");
-    newTask.classList.add("task");
-    newTask.setAttribute("draggable", "true");
-    newTask.innerText = taskName.value;
-
-    lane.appendChild(newTask);
-
-    newTask.addEventListener("click", () => {
-        showDetails(task.title, task.description, task.dueDate, task.conclusionDate || null);
-    });
-    console.log("Calling...");
-    newTask.addEventListener("dragstart", () => {
-        newTask.classList.add("is_dragging");
-    });
-    newTask.addEventListener("dragend", () => {
-        newTask.classList.remove("is_dragging");
-    });
+// Function to close the modal and reset inputs
+function closeModal() {
     resetFormInputs();
-    document.querySelector(".modal_background").style.display = "none";
-};
+    form.removeEventListener("submit", handleCreateTask);
+    form.removeEventListener("submit", handleUpdateTask);
+    modalBackground.style.display = "none";
+}
 
+// Function to reset form inputs
 function resetFormInputs() {
     taskName.value = ""; //Resets the input attribute
     taskDescription.value = "";
@@ -47,7 +35,8 @@ function resetFormInputs() {
     btnSubmit.innerText = "Add Task";
 }
 
-function addCreateTaskEventListener(event) {
+// Function to create a task and add it to the lane
+function handleCreateTask(event) {
     event.preventDefault();
     const value = taskName.value;
 
@@ -74,21 +63,131 @@ function addCreateTaskEventListener(event) {
     });
 }
 
-function showDetails(title, description, dueDate, conclusionDate) {
-    document.querySelector(".modal_background").style.display = "flex";
+// Function to add a task DOM element to the lane
+function addTaskToLane(task) {
+    const newTask = document.createElement("p");
+    newTask.classList.add("task");
+    newTask.setAttribute("draggable", "true");
+    newTask.innerText = taskName.value;
+
+    lane.appendChild(newTask);
+
+    newTask.addEventListener("click", () => {
+        showDetails(task.id, task.title, task.description, formatDate(task.dueDate), task.conclusionDate || null);
+    });
+    console.log("Calling...");
+    newTask.addEventListener("dragstart", () => {
+        newTask.classList.add("is_dragging");
+    });
+    newTask.addEventListener("dragend", () => {
+        newTask.classList.remove("is_dragging");
+    });
+    resetFormInputs();
+    document.querySelector(".modal_background").style.display = "none";
+}
+
+window.addEventListener('load', function() {
+    document.querySelectorAll('.task').forEach(function(taskElement) {
+        taskElement.addEventListener('click', function() {
+            const taskId = taskElement.dataset.taskId;
+            const taskTitle = taskElement.dataset.taskTitle;
+            const taskDescription = taskElement.dataset.taskDescription;
+            const taskDueDate = taskElement.dataset.taskDuedate;
+            const taskConclusionDate = taskElement.dataset.taskConclusiondate;
+
+            console.log("adicionando show details event listener");
+            // taskClickHandler(taskId, taskTitle, taskDescription, taskDueDate, taskConclusionDate);
+            showDetails(taskId, taskTitle, taskDescription, taskDueDate, taskConclusionDate);
+        });
+    });
+});
+
+// Function to show task details in the modal for updating
+function showDetails(id, title, description, dueDate, conclusionDate) {
+    if (dueDate instanceof Object) {
+        dueDate = formatDate(dueDate);
+    }
+
+    openModalDetails(title, description, dueDate);
+    form.removeEventListener("submit", handleCreateTask);
+    form.removeEventListener("submit", handleUpdateTask); // Remove the event listener for updating tasks
+
+    form.addEventListener("submit", handleUpdateTaskOnce); // Add a new event listener for updating tasks only once
+    console.log("adicionando event Listener de atualizacao");
+    function handleUpdateTaskOnce(event) {
+        handleUpdateTask(event, id);
+        form.removeEventListener("submit", handleUpdateTaskOnce); // Remove the event listener after use
+        console.log("removendo event Listener atualizacao");
+    }
+}
+
+// Function to update a task
+function handleUpdateTask(event, taskId) {
+    event.preventDefault();
+    const taskElement = document.getElementById(`task-${taskId}`);
+
+    const value = taskName.value;
+    if (!value) return;
+
+    const formData = {
+        action: "UpdateTask",
+        id: taskId,
+        title: value,
+        description: taskDescription.value,
+        dueDate: taskDueDate.value
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "main",
+        data: formData,
+        success: function (response) {
+            updateTaskElement(response, taskElement);
+            closeModal();
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error);
+            alert("Error occurred trying to save task");
+        }
+    });
+}
+
+// Function to open modal with task details
+function openModalDetails(title, description, dueDate) {
+    modalBackground.style.display = "flex";
     taskName.value = title;
     taskDescription.value = description;
     taskDueDate.value = dueDate;
     btnSubmit.innerText = "Save";
-
-    form.removeEventListener("submit", addCreateTaskEventListener);
-    form.addEventListener("submit", addUpdateTaskEventListener);
 }
 
-//Todo: Chamar controller para update da task
-function addUpdateTaskEventListener(event) {
-    event.preventDefault();
-    console.log("teste");
+// Function to update a taskElement
+function updateTaskElement(response) {
+    const taskElement = document.getElementById(`task-${response.id}`);
+    if (!taskElement) {
+        console.error("Task element not found in DOM");
+        return;
+    }
+    taskElement.textContent = response.title;
+    taskElement.removeEventListener('click', showDetails);
+
+    taskElement.dataset.taskTitle = response.title;
+    taskElement.dataset.taskDescription = response.description;
+    taskElement.dataset.taskDuedate = formatDate(response.dueDate);
+    console.log(taskElement.dataset.taskDuedate);
+    if (response.conclusionDate != null) {
+        taskElement.dataset.taskDuedate = formatDate(response.conclusionDate);
+    }
 }
 
-//Todo: Estilizar o novo modal para detalhes
+function formatDate(dateObj) {
+    const { year, month, day } = dateObj;
+    const formattedMonth = month.toString().padStart(2, '0');
+    const formattedDay = day.toString().padStart(2, '0');
+    return `${year}-${formattedMonth}-${formattedDay}`;
+}
+
+
+//Todo: Refatorar os outros metodos do servlet para a nova configuracao de entityManager
+
+//Todo: Refatorar os testes unitarios 
