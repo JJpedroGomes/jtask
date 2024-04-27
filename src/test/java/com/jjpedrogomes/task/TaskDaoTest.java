d249f9359c96ea264dcc5fcb9615f8f3ac3cfb9c
joao.gomes <joao.gomes2@tjce.jus.br>
2024-04-27 13:53:57 -0300

    [#44] Refactor unit test for taskDao (Create, Delete and Get)

--- src/test/java/com/jjpedrogomes/task/TaskDaoTest.java
+++ src/test/java/com/jjpedrogomes/task/TaskDaoTest.java
@@ -2,93 +2,131 @@ package com.jjpedrogomes.task;
 
 import com.jjpedrogomes.model.task.Task;
 import com.jjpedrogomes.model.task.TaskDao;
-import org.assertj.core.api.AssertionsForClassTypes;
+import com.jjpedrogomes.model.util.JpaUtil;
 import org.junit.jupiter.api.*;
-import org.mockito.InOrder;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceException;
-import javax.persistence.TypedQuery;
-import java.util.Arrays;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static com.jjpedrogomes.task.TaskTest.*;
+import static com.jjpedrogomes.task.TaskTest.buildInProgressTask;
+import static com.jjpedrogomes.task.TaskTest.buildPendingTask;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TaskDaoTest {
 
-    @InjectMocks
     private TaskDao taskDao;
-    @Mock
-    private EntityManager entityManagerMock;
-    @Mock
-    private EntityTransaction transactionMock;
+    private EntityManager entityManager;
     private Task task;
+    private Task taskPreSaved;
+    private int tasksSavedCount;
+    private List<Task> tasksSavedList = new ArrayList<Task>();
+
+    @BeforeAll
+    void createPreSavedTask() {
+        EntityManager entityManager = JpaUtil.getEntityManager();
+        TaskDao taskDaoEnv = new TaskDao(entityManager);
+
+        Task task = buildPendingTask();
+
+        entityManager.getTransaction().begin();
+        taskDaoEnv.save(task);
+        entityManager.getTransaction().commit();
+        taskPreSaved = task;
+        entityManager.close();
+
+        tasksSavedCount++;
+        tasksSavedList.add(taskPreSaved);
+    }
 
     @BeforeEach
-    void setUp() {
-        // Initializes mocks before each test method runs
-        MockitoAnnotations.initMocks(this);
-        // Creates a new task for testing
+    void setUpBeforeEach() {
         this.task = buildInProgressTask();
+        this.entityManager = JpaUtil.getEntityManager();
+        this.taskDao = new TaskDao(entityManager);
+    }
+
+    @AfterEach
+    void setUpAfterEach() {
+        this.entityManager.close();
     }
 
     @Nested
     class task_dao_save {
 
-        /**
-         * Test case for saving a new task.
-         * Verifies that the save operation is performed correctly by mocking EntityManager and EntityTransaction.
-         */
         @Test
         void new_task() {
-            // Arrange
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
             // Act
-            try {
-                taskDao.save(task);
-            } catch (Exception e) {
-                // Ignore
-            }
+            entityManager.getTransaction().begin();
+            taskDao.save(task);
+            entityManager.getTransaction().commit();
             // Assert
-            InOrder inOrder = inOrder(entityManagerMock, transactionMock);
-            inOrder.verify(entityManagerMock).getTransaction();
-            inOrder.verify(transactionMock).begin();
-            inOrder.verify(entityManagerMock).persist(task);
-            inOrder.verify(transactionMock).commit();
+            assertThat(task.getId()).isNotNull();
+
+            tasksSavedCount++;
+            tasksSavedList.add(task);
         }
 
-        /**
-         * Test case for trying to save a task, but an error occurs.
-         * Verifies that the save operation fails gracefully and the transaction is rolled back.
-         */
         @Test
-        void with_error() {
+        void with_error() throws NoSuchFieldException, IllegalAccessException {
             // Arrange
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
-            when(transactionMock.isActive()).thenReturn(true);
-            // Stubbing behavior
-            doThrow(new RuntimeException("Simulated Error")).when(entityManagerMock).persist(task);
-            // Act
-            taskDao.save(task);
-            // Assert
-            verify(entityManagerMock).getTransaction();
-            verify(transactionMock).begin();
-            verify(entityManagerMock).persist(task);
-            verify(transactionMock).rollback();
+            Field title = Task.class.getDeclaredField("title");
+            title.setAccessible(true);
+            title.set(task, null);
+            // Assert and Act
+            entityManager.getTransaction().begin();
+            assertThrows(Exception.class, () -> taskDao.save(task));
+            entityManager.getTransaction().commit();
         }
     }
 
     @Nested
-    class task_dao_get {
+    class task_dao_get_ {
+
+        @Test
+        void by_id_successfully() {
+            // Arrange
+            entityManager.getTransaction().begin();
+            taskDao.save(task);
+            entityManager.getTransaction().commit();
+            // Act
+            Optional<Task> taskFound = taskDao.get(task.getId());
+            // Assert
+            assertThat(taskFound).isPresent();
+            assertThat(taskFound.get()).isEqualTo(task);
+
+            tasksSavedCount++;
+            tasksSavedList.add(task);
+        }
+
+        @Test
+        void by_id_but_task_dont_exist() {
+            // Arrange
+            Long id = 100L;
+            // Act
+            Optional<Task> taskFound = taskDao.get(id);
+            // Assert
+            assertThat(taskFound).isEmpty();
+        }
+
+        @Test
+        void by_id_when_exception_thrown() {
+            // Arrange
+            EntityManager entityManager = mock(EntityManager.class);
+            TaskDao taskDaoEnv = new TaskDao(entityManager);
+            when(entityManager.find(eq(Task.class),anyLong())).thenThrow(new RuntimeException("Simulated Error"));
+            // Act
+            List<Task> taskList = taskDaoEnv.getAll();
+            // Assert
+            assertThat(taskList).isEmpty();
+        }
 
         /**
          * Test case for getting a task list
@@ -97,20 +135,18 @@ class TaskDaoTest {
         @Test
         void non_empty_list() {
             // Arrange
-            Task task1 = buildInProgressTask();
-            Task task2 = buildCompletedTask();
-            Task task3 = buildPendingTask();
-            List<Task> taskList = Arrays.asList(task1, task2, task3);
-            TypedQuery<Task> queryMock = mock(TypedQuery.class);
-            when(entityManagerMock.createQuery(anyString(), eq(Task.class))).thenReturn(queryMock);
-            when(queryMock.getResultList()).thenReturn(taskList);
+            Task newTask = buildPendingTask();
+            entityManager.getTransaction().begin();
+            taskDao.save(newTask);
+            entityManager.getTransaction().commit();
+
+            tasksSavedList.add(newTask);
             // Act
             List<Task> returnedTaskList = taskDao.getAll();
             // Assert
             assertThat(returnedTaskList)
-                    .hasSize(3)
-                    .containsExactlyInAnyOrder(task1, task2, task3);
-            verify(queryMock, times(1)).getResultList();
+                    .hasSize(1 + tasksSavedCount)
+                    .containsExactlyElementsOf(tasksSavedList);
         }
 
         /**
@@ -120,110 +156,111 @@ class TaskDaoTest {
         @Test
         void empty_list() {
             // Arrange
-            TypedQuery<Task> queryMock = mock(TypedQuery.class);
-            when(entityManagerMock.createQuery(anyString(), eq(Task.class))).thenThrow(new PersistenceException("Simulated Error"));
+            EntityManager entityManager = mock(EntityManager.class);
+            TaskDao taskDaoEnv = new TaskDao(entityManager);
+            when(entityManager.createQuery(anyString(), eq(Task.class)))
+                    .thenThrow(new RuntimeException("Simulated Error"));
             // Act
-            List<Task> returnedTaskList = taskDao.getAll();
+            List<Task> taskList = taskDaoEnv.getAll();
             // Assert
-            assertThat(returnedTaskList).isEmpty();
-            verify(entityManagerMock, times(1)).createQuery(anyString(), eq(Task.class));
+            assertThat(taskList).isEmpty();
         }
     }
 
-    @Nested
-    class task_dao_update {
-
-        /**
-         * Test case for updating a task that is not managed by jpa
-         * Verifies that update is not performed
-         */
-        @Test
-        void task_not_managed_by_entity_manager() {
-            // Arrange
-            when(entityManagerMock.contains(any(Task.class))).thenReturn(false);
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
-            // Act and Assert
-            IllegalArgumentException exception = assertThrows(
-                    IllegalArgumentException.class,
-                    () -> taskDao.update(task),
-                    "Task is not managed by the entity manager"
-            );
-            // Assert
-            assertThat(exception.getMessage()).contains("Task is not managed by the entity manager");
-            verify(entityManagerMock).contains(task);
-            verify(entityManagerMock, never()).getTransaction();
-            verify(transactionMock, never()).begin();
-            verify(transactionMock, never()).commit();
-            verify(transactionMock, never()).rollback();
-        }
-
-        /**
-         * Test case for updating a task successfully
-         * Verifies that the transaction behaves correctly
-         */
-        @Test
-        void task_successfully() {
-            // Arrange
-            when(entityManagerMock.contains(any(Task.class))).thenReturn(true);
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
-            // Act
-            taskDao.update(task);
-            // Assert
-            InOrder inOrder = inOrder(entityManagerMock, transactionMock);
-            inOrder.verify(entityManagerMock).getTransaction();
-            inOrder.verify(transactionMock).begin();
-            inOrder.verify(transactionMock).commit();
-        }
-
-        /**
-         * Test case for updating a task with error
-         * Verifies that the transaction rollback occurs
-         */
-        @Test
-        void with_error() {
-            // Arrange
-            when(entityManagerMock.contains(any(Task.class))).thenReturn(true);
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
-            when(transactionMock.isActive()).thenReturn(true);
-            // Stubbing behavior
-            doThrow(new RuntimeException("Simulated Error")).when(transactionMock).commit();
-            // Act
-            RuntimeException exception = assertThrows(
-                    RuntimeException.class,
-                    () -> taskDao.update(task));
-            // Assert
-            verify(transactionMock).rollback();
-            assertThat(exception.getMessage()).contains("Simulated Error");
-        }
-    }
+//    @Nested
+//    class task_dao_update {
+//
+//        /**
+//         * Test case for updating a task that is not managed by jpa
+//         * Verifies that update is not performed
+//         */
+//        @Test
+//        void task_not_managed_by_entity_manager() {
+//            // Arrange
+//            when(entityManagerMock.contains(any(Task.class))).thenReturn(false);
+//            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
+//            // Act and Assert
+//            IllegalArgumentException exception = assertThrows(
+//                    IllegalArgumentException.class,
+//                    () -> taskDao.update(task),
+//                    "Task is not managed by the entity manager"
+//            );
+//            // Assert
+//            assertThat(exception.getMessage()).contains("Task is not managed by the entity manager");
+//            verify(entityManagerMock).contains(task);
+//            verify(entityManagerMock, never()).getTransaction();
+//            verify(transactionMock, never()).begin();
+//            verify(transactionMock, never()).commit();
+//            verify(transactionMock, never()).rollback();
+//        }
+//
+//        /**
+//         * Test case for updating a task successfully
+//         * Verifies that the transaction behaves correctly
+//         */
+//        @Test
+//        void task_successfully() {
+//            // Arrange
+//            when(entityManagerMock.contains(any(Task.class))).thenReturn(true);
+//            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
+//            // Act
+//            taskDao.update(task);
+//            // Assert
+//            InOrder inOrder = inOrder(entityManagerMock, transactionMock);
+//            inOrder.verify(entityManagerMock).getTransaction();
+//            inOrder.verify(transactionMock).begin();
+//            inOrder.verify(transactionMock).commit();
+//        }
+//
+//
+//        /**
+//         * Test case for updating a task with error
+//         * Verifies that the transaction rollback occurs
+//         */
+//        @Test
+//        void with_error() {
+//            // Arrange
+//            when(entityManagerMock.contains(any(Task.class))).thenReturn(true);
+//            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
+//            when(transactionMock.isActive()).thenReturn(true);
+//            // Stubbing behavior
+//            doThrow(new RuntimeException("Simulated Error")).when(transactionMock).commit();
+//            // Act
+//            RuntimeException exception = assertThrows(
+//                    RuntimeException.class,
+//                    () -> taskDao.update(task));
+//            // Assert
+//            verify(transactionMock).rollback();
+//            assertThat(exception.getMessage()).contains("Simulated Error");
+//        }
+//    }
 
     @Nested
     class task_dao_delete {
 
         @Test
         void task_successfully() {
-            // Arrange
-            when(entityManagerMock.merge(any(Task.class))).thenReturn(task);
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
             // Act
-            taskDao.delete(task);
+            entityManager.getTransaction().begin();
+            taskDao.delete(taskPreSaved);
+            entityManager.getTransaction().commit();
             // Assert
-            InOrder inOrder = inOrder(entityManagerMock, transactionMock);
-            inOrder.verify(entityManagerMock).merge(any(Task.class));
-            inOrder.verify(entityManagerMock).remove(task);
+            Optional<Task> optionalDeletedTask = taskDao.get(taskPreSaved.getId());
+            assertThat(optionalDeletedTask).isEmpty();
+
+            tasksSavedList.remove(taskPreSaved);
+            tasksSavedCount--;
         }
 
         @Test
         void task_with_error() {
             // Arrange
-            when(entityManagerMock.getTransaction()).thenReturn(transactionMock);
-            doThrow(new RuntimeException("Simulated Error")).when(entityManagerMock).merge(any(Task.class));
-            // Act & Assert
-            RuntimeException exception = assertThrows(
-                    RuntimeException.class,
-                    () -> taskDao.delete(task));
-            verify(entityManagerMock, never()).remove(task);
-            AssertionsForClassTypes.assertThat(exception.getMessage()).contains("Simulated Error");
+            EntityManager entityManager = mock(EntityManager.class);
+            TaskDao taskDao = new TaskDao(entityManager);
+            // Mocking EntityManager behavior to throw an exception when remove method is called
+            doThrow(RuntimeException.class).when(entityManager).remove(any());
+            // Act and Assert
+            assertThrows(RuntimeException.class, () -> taskDao.delete(taskPreSaved));
         }
     }
 }
