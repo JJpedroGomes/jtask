acc322e3e1f791c6a925f08ade340ae241056b4d
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-05-31 17:54:17 -0300

    [#34] refactor email class

--- src/main/java/com/jjpedrogomes/model/user/Email.java
+++ src/main/java/com/jjpedrogomes/model/user/Email.java
@@ -2,11 +2,23 @@ package com.jjpedrogomes.model.user;
 
 import com.jjpedrogomes.model.shared.ValueObject;
 
+
+/**
+ * The Email class represents an email address as a value object.
+ * It includes validation to ensure the email address is in a valid format.
+ */
 public class Email implements ValueObject<Email>{
 	
 	private String address;
 	private static final String REGEX_EMAIL = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 	
+	/**
+     * Constructs an Email object with the given address.
+     * Validates the email address format before assigning it.
+     * 
+     * @param address The email address to be stored.
+     * @throws RuntimeException if the email address is not valid.
+     */
 	public Email(String address) {
 		if (!isEmailValid(address)) {
 			throw new RuntimeException("Email is not valid");
@@ -18,6 +30,12 @@ public class Email implements ValueObject<Email>{
 		return address;
 	}
 	
+	  /**
+     * Validates the given email address using a regular expression.
+     * 
+     * @param address The email address to validate.
+     * @return true if the email address is valid, false otherwise.
+     */	
 	private boolean isEmailValid(String address) {
 		return address != null && address.matches(REGEX_EMAIL);
 	}
