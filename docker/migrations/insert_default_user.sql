1c75ecacbefd7ce709290e69a5cda608e356958f
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-06-18 10:50:10 -0300

    [#58] Migrations refactor

---
+++ docker/migrations/insert_default_user.sql
@@ -0,0 +1,2 @@
+insert into users (id, name, email, password, birth_date, is_active)
+values (1, 'default', 'emai1l@email.com','$2a$10$3bFaVNzZ4H3EfsfueIugruwxMRFJ/Tanh6XOuUBBk.HODPpu1vw16', '2000-11-15', true);
