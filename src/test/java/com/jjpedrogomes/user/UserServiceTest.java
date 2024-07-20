da8d7eaceb2cc3d81c723da65a16578237fd25ad
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-07-20 20:15:16 -0300

    [#76] Finish tdd development of UserService

--- src/test/java/com/jjpedrogomes/user/UserServiceTest.java
+++ src/test/java/com/jjpedrogomes/user/UserServiceTest.java
@@ -1,16 +1,27 @@
 package com.jjpedrogomes.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.jjpedrogomes.controller.auth.UserDto;
+import com.jjpedrogomes.model.user.Email;
+import com.jjpedrogomes.model.user.Password;
 import com.jjpedrogomes.model.user.User;
 import com.jjpedrogomes.model.user.UserService;
 import com.jjpedrogomes.model.user.UserServiceFactory;
@@ -26,16 +37,32 @@ public class UserServiceTest {
 		this.userService = UserServiceFactory.getInstance();
 	}
 	
+	@Nested
+    @TestMethodOrder(OrderAnnotation.class)
 	class update_user {
 		
+		private String email;
+		private String password;
+		
+		@BeforeAll
+		void setUpBeforaAll() {
+			this.email = "random@email.com";
+			this.password = "abcd@12345";
+			User user = new User("Random Name", new Email(email), new Password(password), LocalDate.of(1974, 9, 27));
+			UserDaoTest.persistUser(user);
+		}
+		
+		@Test
+		@Order(2)
 		void update_user_with_all_params() {
 			// Arrange
-			String name = "newName";
+			String name = "allParam";
 			String birthDate = "1999-08-08";
 			String password1 = "a1b2c3d4";
 			String password2 = password1;
 			UserDto userDto = new UserDto();
 			userDto.setName(name)
+				.setEmail(email)
 				.setBirthDate(birthDate)
 				.setPassword1(password1)
 				.setPassword2(password2);
@@ -47,16 +74,46 @@ public class UserServiceTest {
 			assertTrue(checkIfPasswordMatches(password1, user.getPassword().getContent()));
 		}
 		
+		@Test
+		@Order(1)
 		void update_user_with_few_params() {
-			
+			// Arrange
+			String name = "fewParams";
+			String birthDate = "1999-09-09";
+			UserDto userDto = new UserDto();
+			userDto.setName(name)
+				.setBirthDate(birthDate)
+				.setEmail(email);
+			// Act
+			User user = userService.updateUser(userDto);
+			// Assert
+			assertThat(user.getName()).isEqualTo(name);
+			assertThat(user.getBirthDate().toString()).isEqualTo(birthDate);
+			assertTrue(checkIfPasswordMatches(password, user.getPassword().getContent()));
 		}
 		
+		@Test
+		@Order(3)
 		void update_user_with_invalid_params() {
-			
+			// Arrange
+			String name = "invalid Name 123";
+			UserDto userDto = new UserDto();
+			userDto.setName(name);
+			// Act & Assert
+			assertThrows(IllegalArgumentException.class, () -> userService.updateUser(userDto));
 		}
 		
+		@Test
+		@Order(4)
 		void update_password_but_second_password_is_not_same() {
-			
+			// Arrange
+			String password1 = "a1b2c3d4";
+			String password2 = password1.toUpperCase();
+			UserDto userDto = new UserDto();
+			userDto.setPassword1(password1)
+				.setPassword2(password2);
+			// Act & Assert
+			assertThrows(IllegalArgumentException.class, () -> userService.updateUser(userDto));
 		}	
 	}
 	
