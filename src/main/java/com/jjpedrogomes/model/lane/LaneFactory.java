18e6da98c57a0eba8e133ad2a087e17130b97482
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-08-06 23:19:00 -0300

    [#41] Fix position setting issue

--- src/main/java/com/jjpedrogomes/model/lane/LaneFactory.java
+++ src/main/java/com/jjpedrogomes/model/lane/LaneFactory.java
@@ -6,8 +6,8 @@ public class LaneFactory {
 	
 	public static Lane createLane(String name, User user) {
 		Lane lane = new Lane(name, user);
-        user.getLanes().add(lane);
 		lane.setPosition(user.getLanes().size());
+        user.getLanes().add(lane);
         return lane;
 	}
 }
