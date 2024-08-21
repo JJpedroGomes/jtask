cad631fe0f7418795cc1af6fdce57bb0ba9b7420
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-08-20 23:10:54 -0300

    [#41] Create action for creating lane

---
+++ src/main/java/com/jjpedrogomes/controller/lane/CreateLaneAction.java
@@ -0,0 +1,44 @@
+package com.jjpedrogomes.controller.lane;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.jjpedrogomes.controller.action.Action;
+import com.jjpedrogomes.model.lane.LaneService;
+import com.jjpedrogomes.model.user.User;
+import com.jjpedrogomes.model.user.UserDao;
+
+public class CreateLaneAction implements Action {
+	
+	private final LaneService laneService;
+	private final UserDao<User> userDao;
+	private static final Logger logger = LogManager.getLogger(CreateLaneAction.class);
+	
+	public CreateLaneAction (LaneService laneService, UserDao<User> userDao) {
+		this.laneService = laneService;
+		this.userDao = userDao;
+	}
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			String nameParam = request.getParameter("name");
+			String userEmail = (String) request.getSession().getAttribute("user");
+			
+			if(nameParam == null) {
+				throw new IllegalArgumentException();
+			}
+			
+			userDao.getUserByEmail(userEmail).ifPresent(
+					user -> laneService.createLane(nameParam, user));
+		} catch (RuntimeException e) {
+			logger.info("Unexpected error occured trying to create lane, "
+					+ "check for request parameters");
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
+	}
+
+}
