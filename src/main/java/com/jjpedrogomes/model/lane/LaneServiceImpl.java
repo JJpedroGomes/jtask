37835f2b812200b589d831e86db34a5ce616af49
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-08-20 23:08:35 -0300

    [#41] Create LaneService implementation for the create lane method

---
+++ src/main/java/com/jjpedrogomes/model/lane/LaneServiceImpl.java
@@ -0,0 +1,56 @@
+package com.jjpedrogomes.model.lane;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.jjpedrogomes.model.task.Task;
+import com.jjpedrogomes.model.user.User;
+
+public class LaneServiceImpl implements LaneService {
+	
+	private final LaneDao laneDao;
+	private static final Logger logger = LogManager.getLogger(LaneServiceImpl.class);
+	
+	public LaneServiceImpl(LaneDao laneDao) {
+		this.laneDao = laneDao;
+	}
+
+	@Override
+	public Lane createLane(String name, User user) {
+		logger.info("Creating user...");
+		Lane lane = LaneFactory.createLane(name, user);
+		laneDao.save(lane);
+		return lane;
+	}
+
+	@Override
+	public Lane updateLaneName(Long id, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void switchLanePosition(Long id, int desiredIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeTaskFromLane(Long id, Task Task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewTaskLastToLane(Long id, Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeTaskPositionInLanes(Long currentLaneId, Long desiredLaneId, Long taskId, int desiredIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
