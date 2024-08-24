26de7076dde99a7e955d429a2532aa24df986b41
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-08-24 20:00:00 -0300

    [#41] Create new possibility for this constructor using userDao

--- src/main/java/com/jjpedrogomes/model/lane/LaneServiceFactory.java
+++ src/main/java/com/jjpedrogomes/model/lane/LaneServiceFactory.java
@@ -1,5 +1,7 @@
 package com.jjpedrogomes.model.lane;
 
+import com.jjpedrogomes.model.user.User;
+import com.jjpedrogomes.model.user.UserDao;
 import com.jjpedrogomes.model.util.JpaUtil;
 import com.jjpedrogomes.repository.lane.LaneDaoImpl;
 
@@ -7,5 +9,9 @@ public class LaneServiceFactory {
 
 	public static LaneService getInstance() {
 		return new LaneServiceImpl(new LaneDaoImpl(JpaUtil.getEntityManager()));
+	}
+	
+	public static LaneService getInstance(UserDao<User> userDao) {
+		return new LaneServiceImpl(new LaneDaoImpl(JpaUtil.getEntityManager()), userDao);
 	}
 }
