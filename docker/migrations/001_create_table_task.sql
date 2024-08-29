e7f4b2d0102d83a953a5824e0f5a50ad2811edb9
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-08-28 22:16:39 -0300

    [#41] Change migrations

--- docker/migrations/001_create_table_task.sql
+++ docker/migrations/001_create_table_task.sql
@@ -2,11 +2,12 @@ CREATE SEQUENCE task_sequence START 1;
 
 CREATE TABLE task (
     id BIGINT NOT NULL,
-    title VARCHAR NOT NULL,
+    title VARCHAR(255) NOT NULL,
     description TEXT,
     creation_date DATE NOT NULL,
     due_date DATE,
     conclusion_date DATE,
-    status VARCHAR NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    lane_id BIGINT NOT NULL,
     CONSTRAINT PK_task PRIMARY KEY (id)
 );
