e6350618961e66eb3663e8e4eb16a1d9ce18e8f7
João Pedro Gomes <joaopedrodev00@gmail.com>
2024-09-20 09:26:03 -0300

    [#41] Refactor switch lane position action

--- src/main/java/com/jjpedrogomes/model/lane/LaneFactory.java
+++ src/main/java/com/jjpedrogomes/model/lane/LaneFactory.java
@@ -7,9 +7,9 @@ public class LaneFactory {
 	public static Lane createLane(String name, User user) {
 		Lane lane = new Lane(name, user);
 		
-		int lastPosition = user.getLanes().last().getPosition() + 1;
+		//int lastPosition = user.getLanes().last().getPosition() + 1;
 		
-		lane.setPosition(lastPosition);
+		lane.setPosition(user.getLanes().size());
         user.setLaneToUser(lane);
         return lane;
 	}
